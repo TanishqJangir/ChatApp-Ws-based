284d7e5381a32dfcfd18c42d326e50078db08f1c
TanishqJangir <tanishqjangir001@gmail.com>
2026-01-15 00:20:12 +0530

    Backend for multiple socket

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,1 +1,24 @@
-console.log("Hii there")
+import { WebSocketServer, WebSocket } from "ws";
+
+const ws = new WebSocketServer({port: 8080});
+
+let userCount = 0
+let allSockets : WebSocket[] = [];
+
+ws.on("connection", (socket) => {
+
+    allSockets.push(socket)
+
+    userCount += 1;
+    console.log("User connected #" + userCount);
+
+    socket.on("message", (message)=>{
+        console.log("Message received " + message.toString());
+
+        allSockets.forEach((s) => {
+            s.send(message.toString());
+        })
+    })
+
+
+})
