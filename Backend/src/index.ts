ba1f0c32aa87035a8967df53c30f80574d699d37
TanishqJangir <tanishqjangir001@gmail.com>
2026-01-16 02:29:37 +0530

    completed backend

---
+++ Backend/src/index.ts
@@ -0,0 +1,40 @@
+import { WebSocketServer, WebSocket } from "ws";
+
+const ws = new WebSocketServer({ port: 8080 });
+
+interface User {
+    socket: WebSocket;
+    room: string
+}
+
+let allSockets: User[] = [];
+
+ws.on("connection", (socket) => {
+
+
+    socket.on("message", (message) => {
+        const parsedMessage = JSON.parse(message as unknown as string);
+        if (parsedMessage.type === "join") {
+            allSockets.push({
+                socket,
+                room: parsedMessage.payload.roomId
+            })
+        }
+
+        if (parsedMessage.type === "chat") {
+            const currentUserRoom = allSockets.find((x) => x.socket == socket)?.room;
+
+            allSockets.filter(x => x.room === currentUserRoom)
+                .forEach(x => {
+                    x.socket.send(parsedMessage.payload.message);
+                })
+        }
+    })
+
+
+    socket.on("close", () => {
+        allSockets = allSockets.filter((x) => x.socket != socket)
+    })
+
+
+})
