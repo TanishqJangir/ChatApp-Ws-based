d711834a261b3a518069638318f607a2ab224558
TanishqJangir <tanishqjangir001@gmail.com>
2026-01-17 23:32:48 +0530

    updated fe

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -56,25 +56,36 @@ function App() {
   return (
     <>
       <div className='h-screen bg-black w-screen flex flex-col justify-center'>
+
+
         <div className='h-[95%]'>
+
           <div className='flex gap-2 pl-8 pt-8 flex-col items-start'>
             {messages.map((msg, index) => (
               <div key={index} className='bg-gray-200 p-2 pr-4 rounded-md max-w-[60%] wrap-break-word'>{msg}</div>
             ))}
           </div>
+
         </div>
-        <div className='h-[5%] flex justify-center gap-1'>
+
+
+
+
+        <div className='h-[5%] flex justify-center gap-1 fixed bottom-0 left-1/3'>
+
           <input type='text'
             ref={messageRef}
             placeholder='enter text'
-            className='text-2xl text-white pl-4 rounded-md border-2 outline-none w-[80%]'
+            className='text-2xl text-white pl-4 rounded-md border-2 outline-none'
             onKeyDown={(e) => {
               if (e.key == "Enter") {
                 sendMessage();
               }
             }}
           />
-          <button className='cursor-pointer bg-red-200 rounded-md px-9' onClick={sendMessage}>Send Message</button>
+
+          <button className='cursor-pointer bg-red-200 rounded-md px-9 w-full' onClick={sendMessage}>Send Message</button>
+
         </div>
       </div>
     </>
